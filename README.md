e0296e5c2bef28441aa19b5b47696af0a09d6378
thanhnc2 <thanhnc2@sendo.vn>
2023-08-31 17:31:46 +0700

    update the nearly final script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# dynaR53
+# What is this?
+Because Route53 does not support dynamic updates of the Public IP from a single EC2 instance, this script retrieves the EC2’s public IP and updates the A record to the hosted zone on Route53.
+
+# Requirement
+These environment variables is required for this script work:
+- `INSTANCE_ID`: Instance ID
+- `HOSTEDZONE_ID`: HostedZone ID
+- `SUBDOMAINS`: the list of subdomain you want to update, split by commas `,` .e.g: `"abc,def,gxh"`
+  
--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import (
 	"context"
 	"fmt"
 	"log"
-
-	// "github.com/aws/aws-sdk-go-v2/aws"
+	"strings"
+	"os"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/route53"
+	"github.com/aws/aws-sdk-go-v2/service/route53/types"
 )
 
 // https://www.go-on-aws.com/aws-go-sdk-v2/sdkv2/process/
-var client *ec2.Client
+var ec2Client *ec2.Client
+var r53Client *route53.Client
 
 func init(){
 	// Load the Shared AWS Configuration (~/.aws/config)
@@ -19,30 +23,74 @@ func init(){
 	if err != nil {
 		log.Fatal(err)
 	}
-	client = ec2.NewFromConfig(cfg)
+	ec2Client = ec2.NewFromConfig(cfg)
+	r53Client = route53.NewFromConfig(cfg)
 	
 }
 
 func main() {
-	instanceId := &ec2.DescribeInstancesInput{
-		InstanceIds: []string{"i-0fa921b8db6b34212"},
-	}
+	subs := strings.Split(strings.TrimSpace(os.Getenv("SUB_DOMAIN")), ",")
+	ip := getEc2Ip(strings.TrimSpace(os.Getenv("INSTANCE_ID")))
+	hostedZoneId := strings.TrimSpace(os.Getenv("HOSTEDZONE_ID"))
 	
-	result, err := client.DescribeInstances(context.TODO(),instanceId)
+	setHost(hostedZoneId, ip,subs )
+}
+
+
+func getEc2Ip(iId string) string{
+	instanceId := &ec2.DescribeInstancesInput{
+		InstanceIds: []string{iId},
+	}	
+	result, err := ec2Client.DescribeInstances(context.TODO(),instanceId)
 	if err != nil {
 		panic(err)
 	}
 	count := len(result.Reservations)
 	fmt.Println("Instances: ",count)
+	publicIp := *result.Reservations[0].Instances[0].NetworkInterfaces[0].PrivateIpAddresses[0].Association.PublicIp
+	fmt.Println("Public IP is: ",publicIp)
+	return publicIp
+}
 
-	// log.Print(result)
-	for i, reservation := range result.Reservations {
-		for k, instance := range reservation.Instances {
-			fmt.Println("Instance number: ",i,"-",k	, "Id: ", instance.InstanceId)
-			fmt.Printf("Instance ID: %s, State: %s\n", *instance.InstanceId, instance.State.Name)
-		}
+func getHost(hostedZoneId string )string{
+	input := &route53.GetHostedZoneInput{Id: aws.String(hostedZoneId),}
+	rs, e := r53Client.GetHostedZone(context.TODO(),input)
+	if e != nil {
+		log.Fatal("Cannot get Host")
 	}
+	result := *rs.HostedZone.Name
+	return result
+}
 
+// https://stackoverflow.com/questions/64530843/aws-route53-adding-simple-record
+func setHost(hostedZoneId, ip string, subs []string){
+	hostedZone := getHost(hostedZoneId)
+	for _, v := range subs {
+		input := &route53.ChangeResourceRecordSetsInput{
+			ChangeBatch: &types.ChangeBatch{
+				Changes: []types.Change{
+					{
+						Action: types.ChangeActionUpsert,
+						ResourceRecordSet: &types.ResourceRecordSet{
+							Name: aws.String(v+"."+hostedZone),
+							Type: types.RRTypeA,
+							ResourceRecords: []types.ResourceRecord{
+								{
+									Value: aws.String(ip),
+								},
+							},
+							TTL: aws.Int64(300),
+						},
+					},
+				},Comment: aws.String("Updated by dynaR53"),
+			},
+			HostedZoneId: aws.String(hostedZoneId),
+		}
 
-	// output, err := ec2client.DescribeNetworkInterfaces(context.TODO(), &ec2.DescribeNetworkInterfacesInput{})
+		set, err := r53Client.ChangeResourceRecordSets(context.TODO(), input)
+		if err != nil {
+			log.Panic(err)
+		}
+		fmt.Print(set.ChangeInfo.Status)
+	}
 }

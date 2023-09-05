980984d5445fffe556e9a1c1520ee29e507a2465
thanhnc2 <thanhnc2@sendo.vn>
2023-09-05 16:43:28 +0700

    update to integrate with lambda

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"dynar53/utils"
 	"os"
 	"strings"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func main() {
+func hdlr() {
 	subs := strings.Split(strings.TrimSpace(os.Getenv("SUB_DOMAIN")), ",")
 	ip := utils.GetEc2Ip(strings.TrimSpace(os.Getenv("INSTANCE_ID")))
 	hostedZoneId := strings.TrimSpace(os.Getenv("HOSTEDZONE_ID"))
 
 	utils.SetHost(hostedZoneId, ip, subs)
+}
+
+func main(){
+	lambda.Start(hdlr)
 }
